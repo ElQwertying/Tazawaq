90797b69712b0d5ec91b61203d9286a15ea958b0
ElQwerty <qwer_tyui951753.com>
2026-03-30 23:46:22 +0200

    split and add firebase

---
+++ firebase-config.js
@@ -0,0 +1,21 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBI7lbFVejhxIaAOp2fiYKK0vVGVZJntEY",
+  authDomain: "tazawaq.firebaseapp.com",
+  projectId: "tazawaq",
+  storageBucket: "tazawaq.firebasestorage.app",
+  messagingSenderId: "120461778429",
+  appId: "1:120461778429:web:321c3a039f25bef292cd94",
+  measurementId: "G-5G9ND7LSZZ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
--- state.js
+++ state.js
@@ -1,7 +1,7 @@
 export const state = {
-	idx: 0,
-	dragging: false,
-	startX: 0,
-	startY: 0,
-	startTime: 0
+    idx: 0,
+    dragging: false,
+    startX: 0,
+    startY: 0,
+    startTime: 0
 };
